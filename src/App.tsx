8dba89f18203aef7c52de0793ea1c538407920d8
Mc508 <mihirc508@gmail.com>
2025-06-17 13:58:35 +0530

    signup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
-import Signup from "./pages/SIgnup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 const App = () => {
   return (
-    <Routes>
-      App
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 };
 export default App;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,12 +1,114 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface ISignup {
+  email: string;
+  password: string;
+}
 
 const Signup = () => {
-    const [email,setEmail] = useState()
-    const [password,setPassword] = useState()
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState<ISignup>({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
 
-  return <div>
-    
-    Signup
-    </div>;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/auth/register",
+        {
+          email: formData.email,
+          password: formData.password,
+        }
+      );
+      console.log("registration successfull",response.data);
+      navigate("/login");
+    } catch (error) {
+      console.error("Signup failed:", error);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center my-auto h-screen">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Create your account</CardTitle>
+          <CardDescription>
+            Enter your email password to register
+          </CardDescription>
+          <CardAction>
+            <Button variant="link">Sign Up</Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="m@example.com"
+                  required
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="********"
+                  required
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+                <div className="flex items-center">
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full" onClick={handleSubmit}>
+            Signup
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 export default Signup;
