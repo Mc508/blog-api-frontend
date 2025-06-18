aec760247e04d35a49442570d9cd48a44db5e6bd
Mc508 <mihirc508@gmail.com>
2025-06-18 15:35:50 +0530

    login done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,8 +10,41 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface ILogin {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState<ILogin>({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = async () => {
+    const response = await axios.post(
+      "http://localhost:3000/api/v1/auth/login",
+      {
+        email: formData.email,
+        password: formData.password,
+      }
+    );
+    console.log(response.data);
+    console.log("Login successfully");
+    navigate("/");
+  };
   return (
     <div className="flex items-center justify-center my-auto h-screen">
       <Card className="w-full max-w-sm">
@@ -31,15 +64,25 @@ const Login = () => {
                 <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
+                  name="email"
                   type="email"
+                  value={formData.email}
                   placeholder="m@example.com"
+                  onChange={handleChange}
                   required
                 />
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <Input id="password" type="password" required />
+                  <Input
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    type="password"
+                    required
+                  />
                   <a
                     href="#"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
@@ -52,7 +95,7 @@ const Login = () => {
           </form>
         </CardContent>
         <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
+          <Button type="submit" className="w-full" onClick={handleSubmit}>
             Login
           </Button>
           <Button variant="outline" className="w-full">
